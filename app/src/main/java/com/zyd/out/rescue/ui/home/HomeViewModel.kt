e6c0265c91b648390c38c6f90576cc480e260787
GoodWen <479696877@qq.com>
2021-05-24 11:32:46 +0800

    Initial commit

---
+++ app/src/main/java/com/zyd/out/rescue/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.zyd.out.rescue.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
